856d2731bc3bec57866902d90c9953a1f3c9dffd
LightningYTlol <unconfigured@null.spigotmc.org>
2022-08-17 21:51:45 -0700

    Initial Commit

--- main.sh
+++ main.sh
@@ -4,6 +4,7 @@ source python_env/bin/activate
 pip3 install --upgrade pip && pip3 install requests
 pip3 install -r requirements.txt
 pip3 install --upgrade --no-deps --force-reinstall git+https://github.com/Pycord-Development/pycord
+pip3 install "pymongo[srv]"
 git init
 git remote add origin https://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@github.com/BotsDiscordd/PublicBot.git
 git config user.email "izayikhan@gmail.com"
