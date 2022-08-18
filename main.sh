6008c2b8076cf188d9df9ee2599fa5bda892a571
LightningYTlol <unconfigured@null.spigotmc.org>
2022-08-17 21:47:23 -0700

    Initial Commit

--- main.sh
+++ main.sh
@@ -1,5 +1,6 @@
 pip3 install --upgrade pip && pip3 install requests
 pip3 install -r requirements.txt
+pip3 install --upgrade --no-deps --force-reinstall git+https://github.com/Pycord-Development/pycord
 git init
 git remote add origin https://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@github.com/BotsDiscordd/PublicBot.git
 git config user.email "izayikhan@gmail.com"
