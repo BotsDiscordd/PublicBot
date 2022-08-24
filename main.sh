d6767847d07a672f99496c771abb4e6df5c6e1b0
LightningYTlol <izayikhan@gmail.com>
2022-08-23 21:14:08 -0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,5 +8,5 @@ pip3 install "pymongo[srv]"
 git init
 git remote add origin https://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@github.com/BotsDiscordd/PublicBot.git
 git config user.email "izayikhan@gmail.com"
-git config user.name "LightningYT_lol"
+git config user.name "LightningYTlol"
 python3.8 main.py
