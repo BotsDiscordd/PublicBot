d67965c76581bc242ee6f499d4125e7068da9cc9
LightningYTlol <izayikhan@gmail.com>
2022-08-24 16:10:06 -0700

    test ticket.py??

---
+++ cogs/useless.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+import discord
+
+
+class Useless(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def join(self, ctx, *, channel: discord.VoiceChannel):
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+        await channel.connect()
+
+
+def setup(bot):
+    bot.add_cog(Useless(bot))
